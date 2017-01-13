b0e2d3212eba21053f048c930382bd4a801bf685
Leonardo Faoro <lfaoro@me.com>
2017-01-13 01:38:34 +0100

    created session structure

--- session/session.go
+++ session/session.go
@@ -2,6 +2,7 @@ package session
 
 import (
 	"os"
+	"os/signal"
 	"time"
 )
 
@@ -10,28 +11,52 @@ type Session struct {
 	name     string
 	duration time.Duration
 	delay    time.Duration
-	start    time.Duration
-	end      time.Duration
-	C        os.Signal
-	ticker   time.Ticker
-	timer    time.Timer
+	start    time.Time
+	end      time.Time
+	C        chan os.Signal
+	ticker   *time.Ticker
+	timer    *time.Timer
 }
 
-// New instances a new sessions.
-func New() {
-
+// New instances a new session.
+func New(name string, duration time.Duration, delay time.Duration) *Session {
+	s := &Session{}
+	s.name = name
+	s.duration = duration
+	s.delay = delay
+	s.start = time.Now().Add(delay)
+	s.end = s.start.Add(s.duration)
+	s.C = make(chan os.Signal)
+	return s
 }
 
-// Start runs the session in a goroutine.
-func Start() {
+// Start runs the session.
+func (s *Session) Start() {
+	defer s.Stop()
+	// Once beep
+	// Once smth
+
+	s.ticker = time.NewTicker(time.Second)
+	// goroutine for ticker
+
+	s.timer = time.NewTimer(s.duration)
+	<-s.timer.C
 
 }
 
 // Stop makes sure all goroutines have been stopped.
-func Stop() {
+func (s *Session) Stop() {
+	if s.ticker == nil || s.timer == nil {
+		os.Exit(1)
+	}
+
+	s.ticker.Stop()
+	s.timer.Stop()
 
 }
 
-func restart() {
+func (s *Session) restart() {
+	signal.Notify(s.C, os.Interrupt, os.Kill)
 
+	<-s.C
 }
